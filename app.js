dc00323297deb508ccc4da80e8433aef78734bd0
anildhami49 <102010648+anildhami49@users.noreply.github.com>
2026-01-12 20:04:12 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Backend working fine" });
+});
+
+app.listen(5000, () => {
+  console.log("Backend running on port 5000");
+});
